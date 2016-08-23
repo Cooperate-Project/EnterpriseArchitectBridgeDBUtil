b34dfa2da2baf6613af810f9f76fcca17fba583f
0xSeb <sebah@gmx.de>
2016-08-23 12:35:44 +0200

    Fixed Insert and Delete Trigger Drop.

--- src/main/scala/trigger/Trigger.scala
+++ src/main/scala/trigger/Trigger.scala
@@ -8,7 +8,9 @@ class Trigger(val table: Table, val prefix: String, val exclude: Seq[HibernateTy
 
   def getDropStatements: List[DropStatement] = {
     List(new DropStatement(prefix + table.tableName, DropTypes.TABLE),
-      new DropStatement(prefix + table.tableName + "Trigger", DropTypes.TRIGGER))
+      new DropStatement(prefix + table.tableName + "InsertTrigger", DropTypes.TRIGGER),
+      new DropStatement(prefix + table.tableName + "UpdateTrigger", DropTypes.TRIGGER),
+      new DropStatement(prefix + table.tableName + "DeleteTrigger", DropTypes.TRIGGER))
   }
 
   def getCreateTriggerStatements: List[CreateTriggerStatement] = {
